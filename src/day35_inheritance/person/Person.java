64e781766ccaf96ee2ac74c948424884abfddd44
naima <naimamus36@gmail.com>
2023-04-28 13:09:22 -0400

    day 35 work

--- src/day35_inheritance/person/Person.java
+++ src/day35_inheritance/person/Person.java
@@ -1,5 +1,18 @@
 package day35_inheritance.person;
 
-public class Person {
+//    /Super class -- Parent Class -- Base Class
+    public class Person {
 
-}
+        // instance variable/ fields can inherit
+        String name;
+        int age;
+        String favoriteHobby;
+
+        // instance methods/ actions can inherit
+        public void talk () {
+            System.out.println(name + " is talking");
+        }
+
+    }
+
+
--- src/day35_inheritance/person/Student.java
+++ src/day35_inheritance/person/Student.java
@@ -1,4 +1,19 @@
 package day35_inheritance.person;
 
-public class Student {
-}
+    // Subclass -- Child Class
+// Student is-a Person.
+    public class Student extends Person {
+
+
+        int funLevel;
+
+
+        public void study () {
+            System.out.println(name + " is studying");
+        }
+
+    }
+
+
+
+
