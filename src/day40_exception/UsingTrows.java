7c323cb9f4fcdeb5674f1d3070c0665b77653572
naima <naimamus36@gmail.com>
2023-05-11 18:52:07 -0400

    day 40 exceptions

---
+++ src/day40_exception/UsingTrows.java
@@ -0,0 +1,18 @@
+package day40_exception;
+
+public class UsingTrows {
+    public static void main(String[] args) throws Exception {
+
+//        try {
+//            Thread.sleep(2000);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+
+        Thread.sleep(2000);
+
+
+    }
+
+}
